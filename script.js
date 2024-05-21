771056e5cd7776af804ed37778242434c0c58081
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-05-21 23:28:33 +0300

    begin migration

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let toggle = document.getElementById('theme_toggle');
+document.addEventListener('DOMContentLoaded', () => {
+    /*let toggle = document.getElementById('theme_toggle');
     let storedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
     if (storedTheme) {
         document.documentElement.setAttribute('data-theme', storedTheme)
@@ -18,79 +18,34 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         document.documentElement.setAttribute('data-theme', targetTheme);
         localStorage.setItem('theme', targetTheme);
-    });
+    });*/
     // theme switch
 
-    const sliders = document.querySelectorAll('.slider');
-    const knobs = document.querySelectorAll('.knob');
-    const pointers = document.querySelectorAll('.slider_pointer');
-    const centers = document.querySelectorAll('.slider_center');
+    const parablob1 = document.getElementById('parablob1');
+    const parablob2 = document.getElementById('parablob2');
+    const parablob3 = document.getElementById('parablob3');
+    updateParallax();
+});
 
-    const vert_sliders = document.querySelectorAll('.vertical_slider input');
+function getScrollPercentage() {
+    const { scrollHeight, clientHeight, scrollTop } = document.documentElement;
+    // Alternatively, you can use document.body for the scrollable element
+    const scrolledPortion = scrollHeight - clientHeight - scrollTop;
+    const scrollPerc = scrolledPortion / (scrollHeight - clientHeight);
+    return 1 - Math.min(scrollPerc, 1);
+}
 
-    const color_disps = document.querySelectorAll('.color_disp');
+function lerp(a,b,t) {
+    return (1-t)*a + t*b;
+}
 
-    let dragging = false;
-    let startX, startY;
-    let ind;
-    let angles = [];
+function updateParallax() {
+    const scrollValue = getScrollPercentage();
+    parablob1.style.top = lerp(80, -20, 1.66*scrollValue) + "%";
+    parablob2.style.top = lerp(100, -20, 1.33*scrollValue) + "%";
+    parablob3.style.top = lerp(120, -20, scrollValue) + "%";
+}
 
-    for (let i = 0; i < sliders.length; i++) { angles.push(0); }
-
-    function getAngleFromEvent(event, slider) {
-        const center = {
-            x: slider.clientWidth / 2,
-            y: slider.clientHeight / 2,
-        };
-        const clientX = event.clientX - slider.getBoundingClientRect().left;
-        const clientY = event.clientY - slider.getBoundingClientRect().top;
-        const angle = Math.atan2(clientY - center.y, clientX - center.x) * 180 / Math.PI;
-        return (angle + 360) % 360; // Normalize angle to 0-360 degrees
-    }
-
-    function updateKnobPosition(angle, knob, slider) {
-        const radius = slider.clientWidth / 2;
-        const x = Math.cos(angle * Math.PI / 180) * radius + radius;
-        const y = Math.sin(angle * Math.PI / 180) * radius + radius;
-        knob.style.left = `${x}px`;
-        knob.style.top = `${y}px`;
-    }
-
-    knobs.forEach(key => key.addEventListener('pointerdown', (event) => {
-        dragging = true;
-        startX = event.clientX;
-        startY = event.clientY;
-        ind = Array.from(knobs).indexOf(key);
-    }));
-
-    document.addEventListener('pointerup', () => {
-        dragging = false;
-    });
-
-    function updatePreviewColors(ind) {
-        const color = `hsl(${angles[ind]}, ${vert_sliders[ind].value}%, 50%)`
-        centers[ind].style.backgroundColor = color;
-        color_disps[ind].style.backgroundColor = color;
-        color_disps[ind].style.color = `hsl(${angles[ind]}, ${vert_sliders[ind].value}%, 10%)`;
-    }
-
-    document.addEventListener('pointermove', (event) => {
-        if (dragging) {
-            const newAngle = getAngleFromEvent(event, sliders[ind]);
-            updateKnobPosition(newAngle, knobs[ind], sliders[ind]);
-            angles[ind] = Math.round(Number(newAngle) + 90) % 360;
-            pointers[ind].style.rotate = angles[ind] + 'deg';
-            updatePreviewColors(ind);
-
-            startX = event.clientX;
-            startY = event.clientY;
-        }
-    });
-    // circular sliders
-
-    vert_sliders.forEach(key => key.addEventListener('input', (event) => {
-        key.style = `--lightness: ${key.value}%`;
-        updatePreviewColors(Array.from(vert_sliders).indexOf(key));
-    }))
-    // vertical sliders
-})
+document.addEventListener("scroll", (e) => {
+    updateParallax();
+});
