29791b123948826d8517845dc6ce0c08e819c87a
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-05-23 02:13:34 +0300

    migration fixing 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /*let toggle = document.getElementById('theme_toggle');
+    let toggle = document.getElementById('theme_toggle');
     let storedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
     if (storedTheme) {
         document.documentElement.setAttribute('data-theme', storedTheme)
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         document.documentElement.setAttribute('data-theme', targetTheme);
         localStorage.setItem('theme', targetTheme);
-    });*/
+    });
     // theme switch
 
     const parablob1 = document.getElementById('parablob1');
@@ -32,20 +32,22 @@ function getScrollPercentage() {
     // Alternatively, you can use document.body for the scrollable element
     const scrolledPortion = scrollHeight - clientHeight - scrollTop;
     const scrollPerc = scrolledPortion / (scrollHeight - clientHeight);
-    return 1 - Math.min(scrollPerc, 1);
+    return 1 - Math.max(Math.min(scrollPerc, 1),0);
 }
 
-function lerp(a,b,t) {
+function lerp(a,b,t,capped=true) {
+    if (capped) {
+        t=Math.max(Math.min(t, 1),0);
+    }
     return (1-t)*a + t*b;
 }
 
 function updateParallax() {
     const scrollValue = getScrollPercentage();
-    parablob1.style.top = lerp(80, -20, 1.66*scrollValue) + "%";
-    parablob2.style.top = lerp(100, -20, 1.33*scrollValue) + "%";
-    parablob3.style.top = lerp(120, -20, scrollValue) + "%";
+    parablob1.style.top = lerp(80, -20, 2*scrollValue) + "%";
+    parablob2.style.top = lerp(90, -20, 1.5*scrollValue) + "%";
+    parablob3.style.top = lerp(95, -20, scrollValue) + "%";
 }
 
-document.addEventListener("scroll", (e) => {
-    updateParallax();
-});
+document.addEventListener("scroll", (e) => {updateParallax();});
+// parallax
