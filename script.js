2f56569605820d5072c5226b30e8e2395c4d5a63
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-05-26 01:15:44 +0300

    minor fixes

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ function updateParallax() {
     parablob1.style.top = lerp(80, -20, 2 * scrollValue) + '%';
     parablob2.style.top = lerp(90, -20, 1.5 * scrollValue) + '%';
     parablob3.style.top = lerp(95, -20, scrollValue) + '%';
-    if (scrollValue >= 0.975 && scrollValue <= 1) {
+    if (scrollValue >= 0.965 && scrollValue <= 1) {
         parablob4.style.bottom = 0 + '%';
         document.querySelector('footer').style.opacity = '1';
     } else {
